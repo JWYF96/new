107b7928123b008a64712a7aaced306381cd90fc
John Wong <john6526@gmail.com>
2021-10-30 17:56:26 +0800

    456

--- xyz.py
+++ xyz.py
@@ -5,10 +5,14 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     from selenium import webdriver
-    from selenium.webdriver.common.keys import Keys
     from selenium.webdriver.chrome.options import Options
+
     chrome_options = Options()
     chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    d = webdriver.Chrome('/home/PycharmProjects/chromedriver', chrome_options=chrome_options)
+    d.get('https://www.google.nl/')
     from selenium.webdriver.chrome.service import Service
     from webdriver_manager.chrome import ChromeDriverManager
     from selenium.webdriver.common.by import By
