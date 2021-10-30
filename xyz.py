f091b9aaa322b13bfc274246bc05b4cd63d02e5b
John Wong <john6526@gmail.com>
2021-10-30 17:52:17 +0800

    234

--- xyz.py
+++ xyz.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 def hello_world():
     from selenium import webdriver
     from selenium.webdriver.common.keys import Keys
+    from selenium.webdriver.chrome.options import Options
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
     from selenium.webdriver.chrome.service import Service
     from webdriver_manager.chrome import ChromeDriverManager
     from selenium.webdriver.common.by import By
